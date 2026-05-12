39827cad0106e275cf49e3b6bc21ac1c9e60d76d
Bruno <bruno.fitte@sfr.fr>
2026-05-12 16:46:50 +0200

    Finish script for mariadb and add configuration file

--- srcs/requirements/mariadb/entrypoint.sh
+++ srcs/requirements/mariadb/entrypoint.sh
@@ -14,11 +14,14 @@ if [ ! -d "$DATADIR/mysql" ]; then
     mariadb -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
     mariadb -e "CREATE USER IF NOT EXISTS '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
     mariadb -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO '${SQL_USER}'@'%';"
+    # Privileges MAJ
     mariadb -e "FLUSH PRIVILEGES;"
 
     mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
 
     # mariadb-admin is an administration program for the mariadbd daemon.
+    # Since we just added a password for the root user, we now need to enter it to log in
+    # as root
     mariadb-admin -u root -p"${SQL_ROOT_PASSWORD}" shutdown
 fi
 
